06be8b7905811f23e16853cf07a657fcc4539aa5
Surakshith <surakshith1904@gamil.com>
2026-02-24 22:43:30 +0530

    made a competitor service.js file

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chatController');
 
-// Matches your API spec: POST /chat
 router.post('/', chatController.handleChat);
 
 module.exports = router;
